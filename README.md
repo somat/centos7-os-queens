0a15f7b6a330ba8f4f9d2ce4869ccf590ae269a4
Akhmat Safrudin <somat@artikulpi.com>
2019-09-19 02:12:23 +0700

    Update readme.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # centos7-os-queens
 Install OpenStack Queens on CentOS 7
+
+## Install
+
+1. run controller.sh on controller node
+2. run compute.sh on compute node
+3. run controller-cinder.sh on controller to install cinder service
+4. run storage-cinder.sh to install cinder on storage node
+5. run heat-controller.sh to install heat service on controller
+6. to install swift, check swift/README.md
--- swift/finalize-swift.sh
+++ swift/finalize-swift.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo >/etc/swift/swift.conf <<EOF
+cat >/etc/swift/swift.conf <<EOF
 [swift-hash]
 swift_hash_path_suffix = rahasia
 swift_hash_path_prefix = rahasia
@@ -13,9 +13,12 @@ EOF
 
 chown -R root:swift /etc/swift
 
+# Disable when running on node
+
 systemctl enable openstack-swift-proxy.service memcached.service
 systemctl start openstack-swift-proxy.service memcached.service
 
+# Set service for swift
 systemctl enable openstack-swift-account.service openstack-swift-account-auditor.service \
   openstack-swift-account-reaper.service openstack-swift-account-replicator.service
 systemctl start openstack-swift-account.service openstack-swift-account-auditor.service \
@@ -30,3 +33,6 @@ systemctl enable openstack-swift-object.service openstack-swift-object-auditor.s
   openstack-swift-object-replicator.service openstack-swift-object-updater.service
 systemctl start openstack-swift-object.service openstack-swift-object-auditor.service \
   openstack-swift-object-replicator.service openstack-swift-object-updater.service
+
+# Set SELinux
+chcon -R system_u:object_r:swift_data_t:s0 /srv/node
